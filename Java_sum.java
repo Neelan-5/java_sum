170e8b74319845fd195535cddfd42168ebd83f33
NEELAKANDAN <118263105+Neelan-5@users.noreply.github.com>
2022-12-11 21:35:36 +0530

    Add files via upload

---
+++ Java_sum.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java_sum;
+
+import static java.lang.Integer.sum;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Java_sum {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int i=1;
+    int j=4;
+    int sum=i+j;
+    System.out.println("the sum="+sum);
+    }
+}
